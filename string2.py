9840d0030fd35cf186734d05c5d20328420c6603
Adam Siemionkowicz <adam.siemionkowicz1@marist.edu>
2020-09-17 14:25:50 -0400

    takes first two and last two characters of a string and makes a new string

---
+++ string2.py
@@ -0,0 +1,11 @@
+#string2
+#Python program to get a string made of the first 2 and the last 2 characters from
+#a given a string.
+
+def main():
+
+    str = input("Please type a word: ")
+
+    print(str[ :2] + str[-2: ])
+
+main()

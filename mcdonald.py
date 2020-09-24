7e872f375607baddfb26a8a6240117f8df96d157
Adam Siemionkowicz <adam.siemionkowicz1@marist.edu>
2020-09-24 16:16:26 -0400

    writes lyrics to old mcdonald for any animal

---
+++ mcdonald.py
@@ -0,0 +1,11 @@
+#mcdonald.py
+#prints lyrics to song "Old McDonald"
+
+animal = input("What animal?: ")
+
+def sing():
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+    print("And on the farm he had a",animal,"Ee-igh, Ee-igh, Oh!")
+
+sing()
+    

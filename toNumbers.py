77abe9b5531cecb05b9ad7fa8d6002401a31101f
Adam Siemionkowicz <70294611+asiemz@users.noreply.github.com>
2020-09-25 21:55:54 -0400

    Add files via upload

---
+++ toNumbers.py
@@ -0,0 +1,19 @@
+#toNumbers.py
+#changes entries in a list into numbers
+
+
+
+
+def toNumbers(strList):
+    n = ['1','2','3']
+    newList = str.replace(strList,n)
+    strList = newList
+    print(strList)
+
+
+
+
+
+
+toNumbers("red")
+    

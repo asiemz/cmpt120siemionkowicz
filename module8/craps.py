0b1cc54dc2dba9ef4a8019143e727009bac79f66
Juan Arias <juan.arias@marist.edu>
2020-11-01 22:55:53 -0500

    Feedback

--- module8/craps.py
+++ module8/craps.py
@@ -1,63 +1,53 @@
-#craps.py
+# craps.py
 
 
 from random import randrange
+
+
 def main():
 
     print("Welcome to Craps! In order to win you must roll either a 7 or 11, or you must roll something other than a 2, 3, or 12 twice")
-
 
     x = int(input("Enter the number of times you would like to play: "))
 
     wins = 0
 
+    for i in range(x):
 
-
-    for i in range (x):
-
-        r1 = int(randrange(1,13))
+        r1 = int(randrange(1, 13)) # This is not exactly the same thing, e.g. 1 is not possible
 
         if r1 == 7 or r1 == 11:
             wins = wins + 1
-            print("You win!")
-            
+            #print("You win!")
 
         elif r1 == 2 or r1 == 3 or r1 == 12:
-                print("You lose!")
-                
+            pass
+            #print("You lose!")
 
         else:
-            r2 = int(randrange(1,13))
+            r2 = int(randrange(1, 13))
 
             if r2 == 7:
                 print("You lose!")
-            
 
             elif r2 == r1:
-                    wins = wins + 1
-                    print("You win!")
-                    
+                wins = wins + 1
+                print("You win!")
 
             else:
                 while r2 != r1 or r2 != 7:
-                    r3 = int(randrange(1,13))
+                    r3 = int(randrange(1, 13))
 
                     if r3 == 7 or r3 == r1:
                         wins = wins + 1
                         print("You win!")
-                        
 
                     else:
                         print("You  lose")
                         break
-                        
-
 
         probwin = (wins/x)*100
-        print("Your winning probability is",probwin,"%.")   
+        print("Your winning probability is", probwin, "%.")
 
 
 main()
-        
-
-
--- module8/dice.py
+++ module8/dice.py
@@ -1,41 +1,33 @@
-#dice.py
+# dice.py
 
-#program that performs a simulation to estimate the probability of
-#rolling five of a kind in a single roll of five six-sided dice.
+# program that performs a simulation to estimate the probability of
+# rolling five of a kind in a single roll of five six-sided dice.
 
 
 from random import randrange
+
 
 def main():
     print("This program estimates the probability of rolling five of a kind in one roll of five 6-sided dice")
 
     x = int(input("How many times should we roll? "))
-    
 
+    wins = 0
 
-    for i in range (x):
+    for i in range(x):
 
-          roll1 = randrange(0,7)
-          roll2 = randrange(0,7)
-          roll3 = randrange(0,7)
-          roll4 = randrange(0,7)
-          roll5 = randrange(0,7)
+        roll1 = randrange(1, 7)
+        roll2 = randrange(1, 7)
+        roll3 = randrange(1, 7)
+        roll4 = randrange(1, 7)
+        roll5 = randrange(1, 7)
 
-          if roll1 == roll2 and roll1 == roll3 and roll1 == roll4 and roll1 == roll5:
-              print("You have won")
+        if roll1 == roll2 and roll1 == roll3 and roll1 == roll4 and roll1 == roll5:
+            wins += 1
+            #print("You have won")
 
-          else:
-                print("unsuccessful")
+        #else:
+            #print("unsuccessful")
 
-            
-
+    print("Probability ", wins / x)
 main()
-
-          
-          
-
-    
-
-    
-
-
